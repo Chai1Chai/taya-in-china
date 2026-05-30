4dd2aeef7e2d78b9f02603e910a02e34a3d13ba1
Chai1Chai <aasergienko25102005@gmail.com>
2026-05-30 13:26:53 +0700

    Chenge: domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://taya-in-china.ru'
+  const baseUrl = 'https://tayafromchina.com'
 
   return [
     {
